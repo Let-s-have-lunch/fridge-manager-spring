2a5975390b59abd49836054266685706cb190506
dvpsky <haneulgim95@gmail.com>
2026-08-15 14:31:25 +0900

    초기화

---
+++ src/test/java/com/teamproject/fridgemanagerspring/FridgeManagerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamproject.fridgemanagerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgeManagerSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
